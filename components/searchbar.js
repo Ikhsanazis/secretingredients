cfe518b3003e4909fd569a8c457c0abe600d12d6
unknown <mxnzis@gmail.com>
2022-09-11 16:53:58 -0700

    update search

--- components/searchbar.js
+++ components/searchbar.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import style from "../styles/Home.module.css";
 import { FiSearch } from "react-icons/fi";
 
-function Fixedmenu() {
+function Search() {
   const [data, setData] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState([]);
 
@@ -19,22 +19,22 @@ function Fixedmenu() {
 
   return (
     <>
-            <div className="row mx-2 justify-content-lg-center px-1">
-              <div className="col">
-                <div className={`${style.search} mb-2 mt-1`}>
-                  <form>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Where you want to go?"
-                      required
-                    />
-                    <FiSearch className={style.icon} />
-                  </form>
-                </div>
-              </div>
-            </div>
+      <div className="row mx-2 justify-content-lg-center px-1">
+        <div className="col">
+          <div className={`${style.search} mb-2 mt-1`}>
+            <form>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Where you want to go?"
+                required
+              />
+              <FiSearch className={style.icon} />
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
-export default Fixedmenu;
+export default Search;
