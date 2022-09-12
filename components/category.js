1d3e229b5f28227e09b5274aef3e378881245a45
unknown <mxnzis@gmail.com>
2022-09-12 16:01:39 -0700

    category integration

--- components/category.js
+++ components/category.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import style from "../styles/Home.module.css";
+import Link from "next/link";
 
 function Category() {
   const [data, setData] = React.useState([]);
@@ -18,46 +19,48 @@ function Category() {
 
   return (
     <>
-          <section className={style.popularForYou}>
-            <h3 className="mb-3">Popular for You</h3>
+      <section className={style.popularForYou}>
+        <h3 className="mb-3">Popular for You</h3>
 
-            <div className="row">
-              {[
-                {
-                  name: "Soup",
-                  image: "/image/icon1.png",
-                  link: "/popular/soup",
-                },
-                {
-                  name: "Chicken",
-                  image: "/image/icon2.png",
-                  link: "/popular/soup",
-                },
-                {
-                  name: "Seafood",
-                  image: "/image/icon3.png",
-                  link: "/popular/soup",
-                },
-                {
-                  name: "Dessert",
-                  image: "/image/icon4.png",
-                  link: "/popular/soup",
-                },
-              ].map((item, index) => (
-                <div className="col-3" key={index}>
-                  <div className="d-flex justify-content-center">
-                    <img
-                      src={item?.image}
-                      alt="icon"
-                      width="70px"
-                      height="70px"
-                    />
-                  </div>
-                  <p className="text-center">{item?.name}</p>
+        <div className="row">
+          {[
+            {
+              name: "Soup",
+              image: "/image/icon1.png",
+              link: "/popular/soup",
+            },
+            {
+              name: "Chicken",
+              image: "/image/icon2.png",
+              link: "/popular/soup",
+            },
+            {
+              name: "Seafood",
+              image: "/image/icon3.png",
+              link: "/popular/soup",
+            },
+            {
+              name: "Dessert",
+              image: "/image/icon4.png",
+              link: "/popular/soup",
+            },
+          ].map((item, index) => (
+            <Link href={`/category/${item.name}`} passHref>
+              <div className="col-3" key={index}>
+                <div className="d-flex justify-content-center">
+                  <img
+                    src={item?.image}
+                    alt="icon"
+                    width="70px"
+                    height="70px"
+                  />
                 </div>
-              ))}
-            </div>
-          </section>
+                <p className="text-center">{item?.name}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
---
+++ pages/category/[category].js
@@ -0,0 +1,106 @@
+import Image from "next/image";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { AiFillStar } from "react-icons/ai";
+import { IoChevronBack } from "react-icons/io5";
+import { useSelector } from "react-redux";
+import { decode } from "jsonwebtoken";
+import axios from "axios";
+
+// custom components
+import Link from "next/link";
+
+export default function MyRecipe() {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => state);
+  const decodeUser = decode(auth?.token);
+  const user_id = decodeUser?.id;
+
+  const [myRecipe, setMyRecipe] = React.useState([]);
+  const [loadMy, setLoadMy] = React.useState(true);
+
+  useEffect(() => {
+    getMyRecipes();
+  }, []);
+
+  const getMyRecipes = () => {
+    axios
+      .get(`http://localhost:8000/recipes/${user_id}`)
+      .then((res) => {
+        setMyRecipe(res?.data?.data);
+        setLoadMy(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoadMy(false);
+      });
+  };
+console.log(myRecipe)
+  return (
+    <div id="home" className="container ">
+      <div className="col-lg-4 mx-auto">
+        {/* <MainLayout> */}
+        <div>
+          {myRecipe ? (
+            <section className="">
+              <div className="d-flex">
+                <div className="col-2">
+                  <Link href="/home" passHref>
+                    <IoChevronBack className="fs-3 mt-1 bg-warning" />
+                  </Link>{" "}
+                </div>
+                <h3 className="col-8 text-center">My Recipe</h3>
+              </div>
+              {myRecipe?.map((item) => (
+                <div
+                  className="card"
+                  style={{
+                    borderRadius: "15px",
+                    padding: "10px",
+                    border: "none",
+                    boxShadow: "2px 2px 5px 1px rgba(0,0,0,0.12)",
+                    marginBottom: "20px",
+                    cursor: "pointer",
+                  }}
+                  key={item?.recipe_id}
+                >
+                  <div className="row">
+                    <div className="col-3">
+                      <Image
+                        src={`http://localhost:8000/images/${item?.image}`}
+                        width="80px"
+                        height="80px"
+                        style={{ borderRadius: "16px" }}
+                        alt="image"
+                      />
+                    </div>
+                    <div className="col-9">
+                      <div>
+                        <h6>{item?.name}</h6>
+                        <p>{item?.category}</p>
+                        <div className="d-flex gap-1 align-items-center">
+                          <AiFillStar className="text-warning" />
+                          <span>{item?.LIked ? item?.liked : 0} Like</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </section>
+          ) : (
+            <section>
+              <div>
+                <p>Post a Recipe</p>
+              </div>
+            </section>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
